63638c9b1f5988d13dbfb07ef78ddc1ae028e95e
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-10 01:11:21 -0300

    Created a ViewModel for this project and named it FriendsViewModel  - Properly set up the class so that will act like a MVVM  - Publish a property named friends that is an array of Friend struct.  - Inicialize an object of this class, named friendsVM and set it up so that will be an .environmentObject

---
+++ FriendsGiving/FriendsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FriendsViewModel.swift
+//  FriendsGiving
+//
+//  Created by Christian Manzaraz on 10/01/2024.
+//
+
+import Foundation
+
+class FriendsViewModel: ObservableObject {
+    @Published var friends: [Friend] = []
+    
+    
+    
+}
