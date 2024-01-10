ef931d220d59e34e9480826494bc7fe52c80f2c4
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-10 01:01:24 -0300

    Creating Friend model of own project

---
+++ FriendsGiving/Friend.swift
@@ -0,0 +1,16 @@
+//
+//  Friend.swift
+//  FriendsGiving
+//
+//  Created by Christian Manzaraz on 10/01/2024.
+//
+
+import Foundation
+
+struct Friend: Codable, Identifiable {
+    var id: String?
+    var name = ""
+    var bringing = ""
+    var notes = ""
+    
+}
